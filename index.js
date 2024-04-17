40ce354b2b62af24aed8914b8e92765655f90ec6
Vaggelis-Agapiou <164549406+Vaggelis-Agapiou@users.noreply.github.com>
2024-04-17 15:10:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ for (var i=0; i<document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var buttonInnerHtml = this.innerHTML;
         makeSound(buttonInnerHtml);
+        buttonAnimation(buttonInnerHtml);
 
     });
 }
@@ -10,6 +11,7 @@ for (var i=0; i<document.querySelectorAll(".drum").length; i++) {
 //KEY PRESS DETECTION
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -51,4 +53,12 @@ function makeSound(key) {
 
         default: console.log(buttonInnerHtml);
     } 
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100)
 }
